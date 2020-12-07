0e193809206e12093782dd0a221d3f112414deaa
= <=>
2020-12-07 12:02:25 -0500

    adding post to models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,7 +7,8 @@ from django.utils.translation import ugettext_lazy as _
 class Category(models.Model):
     title = models.CharField(_("title"), max_length=100)
     slug = models.SlugField(_("slug"), db_index=True)
-    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_("root_category"), related_name="category",
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL,
+                               verbose_name=_("root_category"), related_name="category",
                                related_query_name="category")
 
     class Meta:
@@ -16,3 +18,26 @@ class Category(models.Model):
     def __str__(self):
         return f"Your category title is {self.title} ."
 
+
+class Post(models.Model):
+    title = models.CharField(_("title"), max_length=100)
+    slug = models.SlugField(_("slug"), db_index=True, unique=True)
+    content = models.TextField(_('content'), blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE, verbose_name=_('category'),
+                                 related_name='category',
+                                 related_query_name='category')
+    draft = models.BooleanField(_('draft'), default=True, db_index=True)
+    author = models.ForeignKey(User, verbose_name=_('author'), on_delete=models.CASCADE, related_query_name='post',
+                               related_name='post')
+    image = models.ImageField(_('image'), upload_to='images', blank=True, null=True)
+    created_at = models.DateTimeField(_('creation'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('update'), auto_now=True)
+    published_at = models.DateTimeField(_('publish_time'), db_index=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        ordering = ["published_at", "created_at"]
+
+    def __str__(self):
+        return f"{self.title} and its category is {self.category}"
