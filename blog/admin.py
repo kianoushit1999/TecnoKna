f7ef029edba7184b4d29ec2adced9c5e15d1528c
= <=>
2020-12-08 17:04:46 -0500

    adding Category admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class CategoryItemInline(admin.TabularInline):
+    model = Category
+    fields = ('title', 'slug', 'parent')
+    extra = 1
+    show_change_link = True
+
+class postItemInline(admin.TabularInline):
+    model = Post
+    fields = (('title', 'content'), ('author', 'draft'), 'published_at')
+    extra = 1
+    show_change_link = True
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'parent')
+    search_fields = ('title', 'slug')
+    list_filter = ('parent',)
+    list_display_links = ('parent',)
+
+    inlines = [postItemInline, CategoryItemInline]
