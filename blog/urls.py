491cf5b4ad0422fc4035d39a845f02cf0f27b0fb
= <=>
2020-12-09 05:53:08 -0500

    start basic of views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
-    path('blog/', )
+    path('categries/', show_all_categories),
+    path('posts/', show_posts),
+    path('signin/', sign_in),
+    path('sign_up/', sign_up),
+    path('single_post/<slug:pk>', single_post),
+    path('cat_posts/<str:category>', cat_posts)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_all_categories(request):
+    pass
+
+def show_posts(request):
+    pass
+
+def sign_in(request):
+    pass
+
+def sign_up(request):
+    pass
+
+def single_post(request, pk):
+    pass
+
+def cat_posts(request, category):
+    pass
