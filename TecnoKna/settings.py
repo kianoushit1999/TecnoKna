c164a9a989fab3142dbdc3c0b9ea13ea03ce107e
= <=>
2020-12-13 10:03:25 -0500

    addng custome user to setting

--- TecnoKna/settings.py
+++ TecnoKna/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#AuthUserModel
+AUTH_USER_MODEL = 'account.User'
 
 # Application definition
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'account',
 ]
 
 MIDDLEWARE = [
