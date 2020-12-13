f1b7d72a9d286c051a08feb371be4000f1940b25
= <=>
2020-12-13 09:36:30 -0500

    adding models of the user

--- account/models.py
+++ account/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    country = models.CharField(max_length='128', blank=True, null=True)
+
+    def __str__(self):
+        return f'name:{self.username} and country:{self.country}'
+
+class phone(models.Model):
+    phone_re = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Your phone number should be like this format +9999...")
+    phone = models.CharField(verbose_name=_("phone_number"), validators=phone_re, max_length=17, blank=True,
+                             help_text="Enter valid phone number because it'll send you a message at once")
+    owner = models.ForeignKey(
+        verbose_name=_('owner'),
+        to=User,
+        related_name='phone',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'owner:{self.owner} and phone_number:{self.phone}'
