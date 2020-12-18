286e9093872e0d609ff539908dc632b877519bfe
= <=>
2020-12-18 15:39:20 -0500

    compeleting single post and posts view

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('home/', home, name='home'),
                   path('categries/', show_all_categories, name='categories'),
-                  path('posts/', show_posts, name='posts'),
-                  path('single_post/<slug:pk>', single_post, name='post'),
+                  path('posts/', ShowPosts.as_view(), name='posts'),
+                  path('single_post/<slug:slug>', SinglePost.as_view(), name='post'),
                   path('cat_posts/<str:category>', cat_posts, name='category')
             ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
             static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,33 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django.urls import reverse
 from .models import *
 
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'blog/blog.html', context=context)
+
 def show_all_categories(request):
     pass
 
-@login_required
-def show_posts(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'blog/posts.html', context=context)
+@method_decorator(login_required, name='dispatch')
+class ShowPosts(ListView):
+    model = Post
+    template_name = 'blog/posts.html'
 
+class SinglePost(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
 
-def single_post(request, pk):
-    post = Post.objects.get(pk=pk)
-    comments = Comment.objects.filter(post=post)
-    context = {
-        'post': post,
-        'comments': comments
-    }
-    return render(request, 'blog/post.html', context=context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = context.get('post', None)
+        context['comments'] = Comment.objects.filter(post=post)
+        return context
 
 def cat_posts(request, category):
     pass
