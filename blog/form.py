2c8a50b20e34e816b4ad9822fb80f9b4cb93b98c
= <=>
2020-12-22 08:47:15 -0500

    creating comment form in blogform

---
+++ blog/form.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import TextInput, Textarea, CheckboxInput
+from django.utils.translation import gettext_lazy as _
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content', 'situation']
+        widgets = {
+            'content': Textarea(attrs={'placeholder': 'content of your comment'})
+            'situation': CheckboxInput(attrs={})
+        }
