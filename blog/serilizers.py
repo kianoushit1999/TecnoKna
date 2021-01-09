185a126e90db5b1a9dd5f4830e45a964e4886d91
= <=>
2021-01-09 09:57:55 -0500

    updating postSerilizer

--- blog/serilizers.py
+++ blog/serilizers.py
@@ -3,23 +3,17 @@ from account.serilizers import UserSerilizers
 from .models import *
 
 class PostsSerilizers(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=100)
-    slug = serializers.SlugField()
-    content = serializers.CharField()
-    created_at = serializers.DateTimeField(read_only=True)
-    published_at = serializers.DateTimeField()
-
-    def create(self, validated_data):
-        return Post.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.content = validated_data.get('content', instance.content)
-        instance.published_at = validated_data.get('published_at', instance.published_at)
-        instance.save()
-        return instance
+    author_info = UserSerilizers(source='author', read_only=True)
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'content',
+            'author',
+            'category',
+        ]
 
 class CommentSerilizers(serializers.ModelSerializer):
     authors_info = UserSerilizers(source='author', read_only=True)
