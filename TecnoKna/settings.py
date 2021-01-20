d601957cf1dd6bac7e265da9c0ffb57840c883ed
= <=>
2021-01-20 06:54:29 -0500

    adding jwt to working with Tokens to work with api

--- TecnoKna/settings.py
+++ TecnoKna/settings.py
@@ -132,10 +132,12 @@ LOGIN_URL = '/signin/'
 LOGOUT_REDIRECT_URL = '/signin/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
